6cf405211d0334c8fa2092f126541b6e7495df39
lorivalroot <lorivaljunior745@gmail.com>
2025-06-13 22:20:52 -0300

    Teste

--- src/header.php
+++ src/header.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     <header>
-        <h1>Projeto</h1>
+        <h1>Projeto 2</h1>
         <nav>
             <a href="index.php">Início</a> |
             <a href="about.php">Sobre</a> |
